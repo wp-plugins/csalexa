cdb1aa4657a9ddd02da1cb9e8cb5be59cbbae9b9
stephan.gerlach <stephan.gerlach@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-21 20:54:48 +0000

    git-svn-id: https://plugins.svn.wordpress.org/csalexa/trunk@508489 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,80 @@
+<?php
+
+    /* 
+    Plugin Name: csAlexa
+    Description: Adds Alexa verification code to website
+    Author: Stephan Gerlach
+    Version: 1.0 
+    Author URI: http://www.computersniffer.com
+    */  
+    
+    // add admin menu
+    add_action('admin_menu', 'csAlexa_admin_menu');
+    function csAlexa_admin_menu() {
+        add_menu_page('csAlexa', 'csAlexa', 'administrator', 'csAlexa_code', 'csAlexa_code');
+    }
+    
+
+    // options page    
+    function csAlexa_code() {
+        
+        global $wpdb;
+        
+        // check permissions
+        if (!current_user_can('manage_options'))  {
+    	   	wp_die( __('You do not have sufficient permissions to access this page.') );
+    	}
+        
+        // check if update is sent
+        if (isset($_POST['csalexa'])) {
+            
+            // try to get verification key
+            $opt = get_option('csalexa_verification_code');
+            
+            // if verification key exists update key
+            if (!(!$opt) || $opt=='') {
+                update_option('csalexa_verification_code',$_POST['csalexa']);
+            }
+            // if no key exists (first run) insert key
+            else {
+                add_option('csalexa_verification_code',$_POST['csalexa']);
+            }
+            
+        }
+        
+        // load verification key
+        $opt = get_option('csalexa_verification_code');
+        if (!$opt) {
+            $opt = '';
+        }
+        
+        // form output
+    	echo '<div class="wrap">';
+        echo '<h2>Alexa Verification Code</h2>';
+        
+        echo '<form action="" method="post">';
+        echo '<label for="csalexa">Alexa Verification Code</label>: 
+                <input type="text" name="csalexa" id="csalexa" value="'.$opt.'" size="50" />';
+        echo '<br /><input type="submit" name="csalexa_save" value="Save" />';
+        echo '</form>';
+        
+    }
+        
+    
+    // add action for inserting code in <head>
+    add_action('wp_head', 'csAlexa_add_code');
+    
+    function csAlexa_add_code() {
+        
+        global $wpdb;
+        $opt = get_option('csalexa_verification_code');
+        if (!(!$opt)) {
+            echo '<meta name="alexaVerifyID" content="'.$opt.'" />'."\n";
+        }
+        
+    }
+    
+    
+    
+    
+?>
---
+++ readme.txt
@@ -0,0 +1,50 @@
+=== Plugin Name ===
+Contributors: stephan.gerlach
+Plugin URI: http://www.computersniffer.com/csalexa/
+Author: Stephan Gerlach
+Author URI: http://www.computersniffer.com
+Tags: Website analysis
+Requires at least: 3.0.0
+Tested up to: 3.3.1
+Stable tag: 1.0
+
+Automatically adds the meta tag to claim ownership of a site for Alexa
+
+== Description ==
+
+csAlexa adds the meta tag in order to claim ownership of a site for http://www.alexa.com.
+
+Simply get the verification code and save it in the options screen and you are done.
+
+== Installation ==
+
+1. Upload `index.php` to the `/wp-content/plugins/csAlexa` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+* or
+1. Search for csalexa via Plugins - Add New in the admin area
+2. Click on Install
+3. Activate plugin
+
+== Frequently Asked Questions ==
+
+
+= Why do I need this plugin? =
+
+Because if you don't use this plugin you have to manually add the verification code to the template. If you change the template you have to do it again. With this plugin the authentication code is automatically added even if you change theme
+
+
+== Screenshots ==
+1. Simply add the verification code and press save. Done.
+
+== Changelog ==
+
+= 1.0 =
+
+* Initial release
+
+== Upgrade Notice ==
+
+
+
+
+`<?php code(); // goes in backticks ?>`
